4216f43f047a25777f67c0fd24652c4c3be5932a
2164194 <34298969+2164194@users.noreply.github.com>
2018-04-09 21:36:33 +0800

    added script to links folder

---
+++ website/links/script.js
@@ -0,0 +1,37 @@
+var questionLength;
+var submit = function(){
+    length = 0;
+    /*var correct = 0;
+    for(var i = 0; i < document.getElementsByName("q"+i);*/
+    countQuestions();
+    var correct = 0;
+    for(var i = 0; i < questionLength+1; i++){
+        var radio = document.getElementsByName('q'+i);
+        for(var j = 0; j < radio.questionLength; j++){
+            var radioPerQuestion = radio[j];
+            if(radioPerQuestion.value == "c" && radioPerQuestion.checked){
+                correct++;
+            }
+        }
+    }
+    alert("Correct Responses: " + correct);
+    printResult(correct);
+}
+        
+var countQuestions = function() {
+    var formName = document.getElementById("questions");
+    var numOfDiv = formName.getElementsByTagName("div");
+    questionLength = numOfDiv.length;  
+}
+
+var printResult = function(correct){
+    
+    document.getElementById("result").innerHTML = correct;
+    for(var i = 0; i < questionLength; i++){
+        var index = i+1;
+        var x = document.getElementById("answer"+index);
+        x.style.color = 'green';
+        var y = document.getElementById("answer"+index);
+        y.style.fontWeight = "1000";
+    }
+}
