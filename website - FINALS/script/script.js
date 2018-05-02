6502d9596e999605c005892fa3e9de95478300b4
Lmpf <2165944@slu.edu.ph>
2018-05-03 03:58:00 +0800

    responsive menu (still under construction)

--- website - FINALS/script/script.js
+++ website - FINALS/script/script.js
@@ -1,19 +1,19 @@
 var questionLength;
-var submit = function(){
+var submit = function () {
     length = 0;
     /*var correct = 0;
     for(var i = 0; i < document.getElementsByName("q"+i);*/
     countQuestions();
     var correct = 0;
-    for(var i = 0; i < questionLength+1; i++){
-        
-        var radio = document.getElementsByName('q'+i);
+    for (var i = 0; i < questionLength + 1; i++) {
+
+        var radio = document.getElementsByName('q' + i);
         console.log(radio);
-        
-        for(var j = 0; j < radio.length; j++){
-                    console.log("HERE");
+
+        for (var j = 0; j < radio.length; j++) {
+            console.log("HERE");
             var radioPerQuestion = radio[j];
-            if(radioPerQuestion.value == "c" && radioPerQuestion.checked){
+            if (radioPerQuestion.value == "c" && radioPerQuestion.checked) {
                 correct++;
             }
         }
@@ -21,21 +21,30 @@ var submit = function(){
     alert("Correct Responses: " + correct);
     printResult(correct);
 }
-        
-var countQuestions = function() {
+
+var countQuestions = function () {
     var formName = document.getElementById("questions");
     var numOfDiv = formName.getElementsByTagName("div");
-    questionLength = numOfDiv.length;  
+    questionLength = numOfDiv.length;
 }
 
-var printResult = function(correct){
-    
-    document.getElementById("result").innerHTML = "Score: "+correct+" out of "+ questionLength+ " question";
-    for(var i = 0; i < questionLength; i++){
-        var index = i+1;
-        var x = document.getElementById("answer"+index);
+var printResult = function (correct) {
+
+    document.getElementById("result").innerHTML = "Score: " + correct + " out of " + questionLength + " question";
+    for (var i = 0; i < questionLength; i++) {
+        var index = i + 1;
+        var x = document.getElementById("answer" + index);
         x.style.color = 'green';
-        var y = document.getElementById("answer"+index);
+        var y = document.getElementById("answer" + index);
         y.style.fontWeight = "1000";
     }
 }
+
+function dropdownMenu() {
+    var dropdown = document.getElementById("dropdownClick");
+    if (dropdown.className === "topnav") {
+        dropdown.className += " responsive";
+    } else {
+        dropdown.className = "topnav";
+    }
+};
